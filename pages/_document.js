49adc5d8d4abaf647cd0f57cfded0e3356d5143b
Shafiq Belaroussi <shafiq.belaroussi@gmail.com>
2023-07-16 23:40:12 +0100

    check og preview card

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,46 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin = "true" />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap'
-          rel='stylesheet'
-        />
-        <link rel='icon' href='/icon.png'></link>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html>
+			<Head>
+				<link
+					rel='preconnect'
+					href='https://fonts.googleapis.com'
+				/>
+				<link
+					rel='preconnect'
+					href='https://fonts.gstatic.com'
+					crossOrigin='true'
+				/>
+				<meta
+					property='og:title'
+					content="Shafiq Belaroussi's Public projects"
+				/>
+				<meta
+					property='og:description'
+					content="Some of my public projects that I've worked on during my free time."
+				/>
+				<meta
+					property='og:url'
+					content='https://www.shafiqbelaroussi.co.uk/'
+				/>
+				<meta
+					property='og:image'
+					content='https://media.discordapp.net/attachments/1006937813712904202/1130254505028632617/roundeD.jpg'
+				/>
+				<link
+					href='https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap'
+					rel='stylesheet'
+				/>
+				<link
+					rel='icon'
+					href='/icon.png'></link>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
